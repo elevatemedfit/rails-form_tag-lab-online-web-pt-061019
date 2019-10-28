929af9a7b9f56536bcde661976a99b2e59b30f9d
Chris Junker <elevatemedfit@gmail.com>
2019-10-28 13:00:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,14 +4,6 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
-  def new
-  end
-
-  def create
-    Student.create(name: params[:student][:name])
-    redirect_to students_path
-  end
-
   def show
     @student = Student.find(params[:id])
   end
